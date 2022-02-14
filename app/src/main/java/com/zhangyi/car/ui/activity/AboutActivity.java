40302ad324b3cc0fad4c839054956bfd8adc3934
zhangyi <zhangyi22@kingsoft.com>
2022-02-14 15:40:01 +0800

    修改包名

---
+++ app/src/main/java/com/zhangyi/car/ui/activity/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.zhangyi.car.ui.activity;
+
+import com.zhangyi.car.R;
+import com.zhangyi.car.app.AppActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 关于界面
+ */
+public final class AboutActivity extends AppActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.about_activity;
+    }
+
+    @Override
+    protected void initView() {}
+
+    @Override
+    protected void initData() {}
+}
